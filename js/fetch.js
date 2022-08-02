ede0bb1590297b9286594ce09256a628a3dc8e94
husamkamal <husamkmal94@gmail.com>
2022-08-02 15:54:17 +0300

    create fetch function that take tow parameter api url and callbake function

---
+++ js/fetch.js
@@ -0,0 +1,18 @@
+const xhr =new XMLHttpRequest()
+
+const fetch=(url,cb)=>{
+    xhr.onreadystatechange=()=>{
+        if(xhr.readyState==4){
+            if(xhr.status==200){
+                const data=JSON.parse(xhr.responseText)
+                cb(data)    
+            }else{
+                console.log(error,)
+            }
+        }
+    }
+    xhr.open('GEt',url)
+    xhr.send()
+}
+
+fetch(url,dat)
