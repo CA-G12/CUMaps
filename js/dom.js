af082a7107270e561506582d1b7e39d261cd2b3e
Mai elhajeen <maielhajeen2001@gmail.com>
2022-08-03 17:01:08 +0300

    chang

--- js/dom.js
+++ js/dom.js
@@ -72,7 +72,7 @@ const createCard = (data) => {
     cardFooter.appendChild(title);
     const anchor = document.createElement("a");
     anchor.className = "button card-button";
-    anchor.href = "../uneversty/index.html";
+    anchor.href = "./uneversty/index.html";
     cardFooter.appendChild(anchor);
 
     anchor.addEventListener("click", (e) => {
